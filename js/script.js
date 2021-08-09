87f4fd13753ff546fcff4896534c5b6ad8157237
Nirajan Mahara <mnirajan9@gmail.com>
2021-08-09 16:49:58 +0545

    ➕ Add `function getJobs() {}` to fetch the data from the `data.json` and convert response to `.json()` object and return `data`

--- js/script.js
+++ js/script.js
@@ -2,3 +2,14 @@ document.querySelector('.button-container').addEventListener('click', () => {
   let text = document.getElementById('filter-jobs').value;
   console.log(text, '⌨ Value');
 });
+
+function getJobs() {
+  fetch('data.json')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      return data;
+    });
+}
+
+getJobs();
