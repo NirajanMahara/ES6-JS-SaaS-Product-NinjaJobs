303408f1839622f5b91e17e493aa2101e00a477a
Nirajan Mahara <mnirajan9@gmail.com>
2021-08-10 10:03:21 +0545

    ➕ Add the `function filterJobs(jobs, searchText)` to filter the array `let filteredJobs = jobs.filter((job) => {}` using `.toLowerCase().includes(searchText)` in the JSON fields & `return filteredJobs;`

--- js/script.js
+++ js/script.js
@@ -11,6 +11,26 @@ function getJobs() {
     });
 }
 
+function filterJobs(jobs, searchText) {
+  if (searchText) {
+    let filteredJobs = jobs.filter((job) => {
+      if (
+        job.roleName.toLowerCase().includes(searchText) ||
+        job.type.toLowerCase().includes(searchText) ||
+        job.company.toLowerCase().includes(searchText) ||
+        job.requirements.content.toLowerCase().includes(searchText)
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    return filteredJobs;
+  } else {
+    return jobs;
+  }
+}
+
 function showJobs(jobs) {
   // console.log('💼 Jobs in showJobs', jobs);
 
